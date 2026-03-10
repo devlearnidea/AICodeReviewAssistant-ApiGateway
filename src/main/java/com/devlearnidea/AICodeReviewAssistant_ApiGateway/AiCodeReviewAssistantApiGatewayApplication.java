2147e4164cd31b762682bb932a3ce4cff46f953a
Manikandan <manikandan61203@gmail.com>
2026-03-10 15:43:52 +0530

    [INITIAL][SETUP] : inital setup

---
+++ src/main/java/com/devlearnidea/AICodeReviewAssistant_ApiGateway/AiCodeReviewAssistantApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.devlearnidea.AICodeReviewAssistant_ApiGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeReviewAssistantApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodeReviewAssistantApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlearnidea/AICodeReviewAssistant_ApiGateway/AiCodeReviewAssistantApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlearnidea.AICodeReviewAssistant_ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeReviewAssistantApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
